57311d49d97fd436b86a9bfbae7613b75ba02c27
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-07-16 23:56:12 -0400

    update

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ def finalGrade(resitation, test, exam):
 
   return final
 
+def finalGrade2(res1, res2, test, exam):
+  resitation = ((res1+res2)*5)/20
+
+  final = test + resitation + exam
+
+  return final
+
 if __name__ == '__main__':
   run = ""
   while run != "x":
-    res = float(input("resitation: "))
+    res1 = float(input("resitation 1: "))
+    res2 = float(input("resitation 2: "))
     test = float(input("Test d'evaluation: "))
     exam = float(input("Examen: "))
 
-    print(finalGrade(res, test, exam))
+    print(finalGrade2(res1, res2, test, exam)*150/100)
     run = input("continue or not?  ")
     print("")
